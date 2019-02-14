74d428d2119a7fc389e4e00d821fc80b802166c0
rick <rickwestdev@gmail.com>
2019-02-14 12:26:33 +0000

    core ui

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,9 +6,16 @@
  */
 
 // any CSS you require will output into a single css file (app.css in this case)
+
+// Core UI CSS
+require("@coreui/coreui/dist/css/coreui.min.css");
+// Custom CSS
 require('../css/app.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+// Core UI Javascript
+require('popper.js/dist/umd/popper.min.js');
+require('bootstrap/dist/js/bootstrap.min.js');
+require('@coreui/coreui/dist/js/coreui.min.js');
