7c4a87888b8efe15d2695b91c27d17f2fef6ecc3
rick <rickwestdev@gmail.com>
2019-02-19 23:40:11 +0000

    use sass

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,16 +7,8 @@
 
 // any CSS you require will output into a single css file (app.css in this case)
 
-// Icons
-require('@coreui/icons/css/coreui-icons.min.css');
-require('flag-icon-css/css/flag-icon.min.css');
-require('@fortawesome/fontawesome-free/css/all.min.css');
-require('simple-line-icons/css/simple-line-icons.css');
-
-// Core UI CSS
-require("@coreui/coreui/dist/css/coreui.min.css");
-// Custom CSS
-require('../css/app.css');
+// Styles
+require('../scss/app.scss');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 const $ = require('jquery');
