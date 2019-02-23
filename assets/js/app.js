9917067c71199337dc35033eb7ac7daa74df0ce8
rick <rickwestdev@gmail.com>
2019-02-23 01:52:35 +0000

    update yarn deps

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,4 +16,5 @@ const $ = require('jquery');
 // Core UI Javascript
 require('popper.js/dist/umd/popper.min.js');
 require('bootstrap/dist/js/bootstrap.min.js');
+require('perfect-scrollbar/dist/perfect-scrollbar.min.js');
 require('@coreui/coreui/dist/js/coreui.min.js');
